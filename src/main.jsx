9e8590a60a186ee4320ad8c37ab3f424a54f081d
Andrade <comanderandrade@gmail.com>
2024-11-25 20:07:52 -0300

    header e footer adicionados na main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css"; // Importa o CSS do Bootstrap
 import './index.css' // Importa o CSS personalizado
 import App from './App.jsx'
+import Header from './components/navbar/header.jsx';
+import Footer from './components/footer/footer.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Header />
     <App />
+    <Footer />
   </StrictMode>,
 )
