ea515a9ebace9947d2cf8455a3ad41e4dfbce21f
Andrade <comanderandrade@gmail.com>
2024-11-25 19:53:35 -0300

    bootstrap instalado, header e footer criados

---
+++ src/components/navbar/header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header>
+      <h1>Meu App</h1>
+      <nav>
+        <a href="/">Home</a>
+        <a href="/about">Sobre</a>
+        <a href="/whatwedo">O que fazemos</a>
+        <a href="/contact">Contato</a>
+      </nav>
+    </header>
+  );
+}
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import "bootstrap/dist/css/bootstrap.min.css"; // Importa o CSS do Bootstrap
+import './index.css' // Importa o CSS personalizado
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
