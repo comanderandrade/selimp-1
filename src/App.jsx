3b6ab71f1e58fdbcdbdeaab58c226d2a69ed55df
Andrade <comanderandrade@gmail.com>
2024-11-26 17:05:31 -0300

    criação dos componentes e começo do description

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import Header from './components/header/header'
 import Footer from './components/footer/footer'
+import Home from './components/home/home'
+import Description from './components/description/description'
+import OQueFazemos from './components/o-que-fazemos/oquefazemos'
+import QuemSomos from './components/quem-somos/quemSomos'
+import Contato from './components/contato/contato'
+
 import './App.css'
 
 function App() {
   return (
     <>
         <Header />
-
+        <Home />
+        <Description />
+        <OQueFazemos />
+        <QuemSomos />
+        <Contato />
         <Footer />
     </>
   )
