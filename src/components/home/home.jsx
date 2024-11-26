c5587d9ffa43d88b6d2517f4dd27d56e282afff9
Andrade <comanderandrade@gmail.com>
2024-11-26 19:43:36 -0300

    Home "quase" pronto, falta alinhar o texto

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,13 +1,18 @@
 import styles from './home.module.css';
-function Home() {
+import fundo from "../../assets/fundo.png";
+import person from "../../assets/person.png";
 
+function Home() {
   return (
-    <div>
-      <h1>Soluções inteligentes para condomínios!</h1>
-      <h2>Conheça nossos serviços!</h2>
-      <a href="" alt="babaca-png"></a>
+    <div className={styles.containerBanner}>
+      <img src={fundo} alt="Fundo" className={styles.backgroundImage} />
+      <div className={styles.textContent}>
+        <h1>Soluções inteligentes para condomínios!</h1>
+        <h2>Conheça nossos serviços!</h2>
+      </div>
+      <img src={person} alt="Pessoa" className={styles.personImage} />
     </div>
   );
-
 }
+
 export default Home;
